26a9ffd73f3fd4d4efe38a268dc536aefd33f3e9
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-09-23 18:15:03 -0300

    feat: adiciona a classe Archetype.

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _special: number;
+  private _cost: number;
+
+  constructor(
+    private _name: string,
+  ) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
